0bc04063c372db9d8680d4546e375c1d5fd2788e
Mehdou MBULA <contact@boomrang-group.com>
2025-10-08 15:56:52 +0000

    /** @type {import('next').NextConfig} */ const nextConfig = {   typescri

--- src/next.config.ts
+++ src/next.config.ts
@@ -10,13 +10,19 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    domains: [
+      'www.boomrang-group.com',
+      'boomrang-group.com',
+      'i0.wp.com',
+      'i1.wp.com',
+      'i2.wp.com',
+    ],
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'www.boomrang-group.com',
-        port: '',
-        pathname: '/**',
-      }
+      { protocol: 'https', hostname: 'www.boomrang-group.com', pathname: '/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'boomrang-group.com',      pathname: '/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'i0.wp.com', pathname: '/www.boomrang-group.com/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'i1.wp.com', pathname: '/www.boomrang-group.com/wp-content/uploads/**' },
+      { protocol: 'https', hostname: 'i2.wp.com', pathname: '/www.boomrang-group.com/wp-content/uploads/**' },
     ],
   },
   webpack: (config, { isServer }) => {
